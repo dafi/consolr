04c0c03c5c08419ec45d97ebc1c2d617c45335ae
Davide Ficano <davide.ficano@gmail.com>
2010-04-25 14:54:48 +0200

    Fixed issue 4 - Calling login page when already logged should not ask for credential

--- webapp/login.php
+++ webapp/login.php
@@ -1,4 +1,10 @@
 <?php
+require_once 'lib/loginUtils.php';
+
+if (login_utils::is_logged()) {
+    header ("location: queue.php");
+    return;
+}
 echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
