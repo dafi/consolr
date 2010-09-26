8a943eee0ae70740a9eb8925ae0340fde7469717
Davide Ficano <davide.ficano@gmail.com>
2010-09-26 16:49:44 +0200

    moved tumblr name on the left

--- webapp/inc/menu.php
+++ webapp/inc/menu.php
@@ -5,6 +5,8 @@ require_once 'lib/tumblr/tumblrUtils.php';
 ?>
         <div class="common-menu">
             <?php if (login_utils::is_logged()) { ?>
+            <span class="tumblr-name"><?php echo login_utils::get_tumblr()->get_tumblr_name() ?></span>
+            |
             <a href="home.php">Home</a>
             |
             <a href="multiq.php">Photo Uploader</a>
@@ -13,7 +15,7 @@ require_once 'lib/tumblr/tumblrUtils.php';
             |
             <a href="http://www.tumblr.com/tumblelog/<?php echo login_utils::get_tumblr()->get_tumblr_name() ?>" target="_blank"><img width="15" height="13" src="images/external_link.png" alt="" style="vertical-align: middle"/> Dashboard</a>
             |
-            <a href="logout.php">Logout [<?php echo login_utils::get_tumblr()->get_tumblr_name() ?>]</a>
+            <a href="logout.php">Logout</a>
             <?php } else { ?>
             <a href="login.php">Login</a>
             <?php } ?>
