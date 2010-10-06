d7ca250a69d798d116fe231c34a0a39b2cce386f
Davide Ficano <davide.ficano@gmail.com>
2010-10-06 17:09:08 +0200

    save also publish time

--- webapp/tags.php
+++ webapp/tags.php
@@ -95,12 +95,12 @@ echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
                         var tag = tags[j];
 
                         if (typeof (tagsMap[tag]) == 'undefined') {
-                            tagsMap[tag] = [post.id];
+                            tagsMap[tag] = [{id: post.id, ts: post['unix-timestamp']}];
                         } else {
                             if (tagsMap[tag].indexOf(post.id) >= 0) {
                                 if (window.console) console.error('Found duplicated tag "' + tag + '" for post ' + post.id);
                             } else {
-                                tagsMap[tag].push(post.id);
+                                tagsMap[tag].push({id: post.id, ts: post['unix-timestamp']});
                             }
                         }
                     }
