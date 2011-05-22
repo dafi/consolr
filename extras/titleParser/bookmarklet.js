71ef9e1654162a74f3bd774900154f03b5fb2b86
Davide Ficano <davide.ficano@gmail.com>
2011-05-22 10:58:07 +0200

    bookmarklet pointing to external site

---
+++ extras/titleParser/bookmarklet.js
@@ -0,0 +1,1 @@
+javascript:if (typeof(consolrTitleParser) == 'undefined'){try{var%20s=document.createElement('script');s.setAttribute('src','http://www.consolr.netsons.org/js/titleParser.js');document.getElementsByTagName('body')[0].appendChild(s);}catch(e){alert(e)}}else{consolrTitleParser.fill();};void(s);
