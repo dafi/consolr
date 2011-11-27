60c8f481d38ed37b16bea2555910d0da9b54d515
Davide Ficano <davide.ficano@gmail.com>
2011-11-27 12:52:09 +0100

    added view

--- db/mysql/CONSOLR_BIRTHDAY_ddl.sql
+++ db/mysql/CONSOLR_BIRTHDAY_ddl.sql
@@ -5,3 +5,11 @@ CREATE TABLE IF NOT EXISTS CONSOLR_BIRTHDAY (
   tumblr_name VARCHAR( 255 ) NOT NULL ,
   PRIMARY KEY (id)
 ) ENGINE=InnoDB;
+
+CREATE VIEW CONSOLR_VW_MISSING_BIRTHDAYS AS
+    select distinct t.TAG AS name from CONSOLR_POST_TAG t
+    where ((t.SHOW_ORDER = 1)
+        and (t.TUMBLR_NAME = '')
+        and (not(t.TAG in (select CONSOLR_BIRTHDAY.name from CONSOLR_BIRTHDAY)))
+        and (t.TAG not in ('art')))
+    order by t.TAG
