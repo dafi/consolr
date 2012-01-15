58f37150350cdef3cb2f8e85d1ac84b9a9bac03d
Davide Ficano <davide.ficano@gmail.com>
2012-01-15 12:19:23 +0100

    open links to _blank

--- webapp/inc/menu.php
+++ webapp/inc/menu.php
@@ -5,15 +5,15 @@ require_once 'lib/tumblr/tumblrUtils.php';
 ?>
         <div class="common-menu">
             <?php if (login_utils::is_logged()) { ?>
-            <span class="tumblr-name"><?php echo login_utils::get_tumblr()->get_tumblr_name() ?></span>
+            <a href="http://<?php echo login_utils::get_tumblr()->get_tumblr_name() ?>.tumblr.com" target="_blank" class="tumblr-name"><?php echo login_utils::get_tumblr()->get_tumblr_name() ?></a>
             |
             <a href="home.php">Home</a>
             |
-            <a href="upload.php">Photo Uploader</a>
+            <a href="upload.php" target="_blank">Photo Uploader</a>
             |
-            <a href="queue.php">Queue</a>
+            <a href="queue.php" target="_blank">Queue</a>
             |
-            <a href="queue.php?state=d">Draft</a>
+            <a href="queue.php?state=d" target="_blank">Draft</a>
             |
             <a href="http://www.tumblr.com/tumblelog/<?php echo login_utils::get_tumblr()->get_tumblr_name() ?>" target="_blank"><img width="15" height="13" src="images/external_link.png" alt="" style="vertical-align: middle"/> Dashboard</a>
             |
